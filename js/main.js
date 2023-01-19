ad256488be833b3f4a9b334bc1eac1c1381666b1
Tsai fu lien <zxcv1995zz@gmail.com>
2023-01-20 01:03:38 +0800

    final
    [task] add web

---
+++ js/main.js
@@ -0,0 +1,53 @@
+let search = document.querySelector(".search-box");
+document.querySelector("#search-icon").onclick = () => {
+  search.classList.toggle("active");
+  cart.classList.remove("active");
+  user.classList.remove("active");
+};
+
+let cart = document.querySelector(".cart");
+document.querySelector("#cart-icon").onclick = () => {
+  cart.classList.toggle("active");
+  search.classList.remove("active");
+  user.classList.remove("active");
+};
+
+let user = document.querySelector(".user");
+document.querySelector("#user-icon").onclick = () => {
+  user.classList.toggle("active");
+  search.classList.remove("active");
+  cart.classList.remove("active");
+};
+
+var swiper = new Swiper(".new-arrival", {
+  spaceBetween: 20,
+  loop: true,
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 0,
+    },
+    568: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
